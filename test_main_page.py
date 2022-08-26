61edc40948b9d6af91535bf1e41ef58fae953f94
Stas Krasilnikov <kexs888@yandex.ru>
2022-08-26 14:15:59 +0500

    Added: locators.py, login_page.py for checking elements on page.

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 import time
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_to_login_page(browser):
@@ -7,8 +8,10 @@ def test_guest_can_to_login_page(browser):
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     time.sleep(2)
-    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
-    time.sleep(2)
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
